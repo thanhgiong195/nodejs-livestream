142394c147d0d89c5e2856efc1e29c56e8bf4f97
ThanhGiong <giongbt@gmail.com>
2022-06-26 16:00:08 +0700

    update code

--- public/script.js
+++ public/script.js
@@ -42,7 +42,7 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
   const video = document.createElement('video');
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream);
+    // addVideoStream(video, userVideoStream);
   });
   call.on('close', () => {
     video.remove();
