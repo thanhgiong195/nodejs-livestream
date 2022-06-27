62f5b3af22b863bb382206f9176fd47ca64ac308
ThanhGiong <giongbt@gmail.com>
2022-06-27 07:17:48 +0700

    Update code

--- server.js
+++ server.js
@@ -3,20 +3,34 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+//const { ExpressPeerServer } = require('peer');
+
+//const peerServer = ExpressPeerServer(server, {
+//  debug: true
+//});
 
 app.set('view engine', 'ejs');
+//app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`);
+  //res.redirect(`/${uuidV4()}`);
+  res.redirect(`/room1`);
 });
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
 
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+
 io.on('connection', socket => {
+	console.log('connection server'); 
+	console.log(socket.id);
   socket.on('join-room', (roomId, userId) => {
+	console.log('join-room server', roomId, userId); 
     socket.join(roomId);
     socket.to(roomId).emit('user-connected', userId);
 
